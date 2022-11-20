56219213e935e7f32cb1882e646249b318da4010
ValeraDanger <oleg.petrov.03@list.ru>
2022-11-20 20:50:10 +0300

    Исправление кодировки

--- SortLab/ArrayTools.h
+++ SortLab/ArrayTools.h
@@ -1,4 +1,4 @@
-#pragma once
+п»ї#pragma once
 
 void swap(int* elem1, int* elem2);
 void printArray(int* arr, int size);
@@ -7,4 +7,4 @@ int findMin(int* arr, int size);
 void copyArray(int* arr, int* copying_arr, int size);
 void generateRandomArray(int* arr, int size);
 void manualArrayFilling(int* arr, int size);
-void merge(int* arr, int left, int mid, int right);	/*Объединяет в arr отсортированные элементы из [arr[left], arr[mid) с отсортированными элементами из [arr[mid], arr[right])*/
+void merge(int* arr, int left, int mid, int right);	/*РћР±СЉРµРґРёРЅСЏРµС‚ РІ arr РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Рµ СЌР»РµРјРµРЅС‚С‹ РёР· [arr[left], arr[mid) СЃ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹РјРё СЌР»РµРјРµРЅС‚Р°РјРё РёР· [arr[mid], arr[right])*/
